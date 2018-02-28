e02426de36ed2604b2f5d69a7d5b2a5ea4bbe9e8
IvanLu1024 <501275190@qq.com>
2018-02-28 19:16:36 +0800

    利用SparkSQL从本地读取文件 写入MySQL数据库

---
+++ spark/utils/connectDataBaseUtil.scala
@@ -0,0 +1,19 @@
+package com.shixun.spark.utils
+
+import java.util.Properties
+
+import org.apache.spark.sql.{DataFrame, SaveMode}
+
+object connectDataBaseUtil {
+
+  def connect(dataFrame:DataFrame, tableName:String):Unit={
+    //创建properties存储数据库相关属性
+    val prop = new Properties()
+    prop.put("user","root")
+    prop.put("password","123456")
+    //将数据追加到数据库
+    dataFrame.write.mode(SaveMode.Append).jdbc("jdbc:mysql://localhost:3306/big_data","big_data."+tableName,prop)
+
+  }
+
+}
